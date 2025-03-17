2ff88775ff55c21126fb77a82c06b15196bfc973
pavrudakov <rudakov2005@mail.ru>
2025-03-17 16:32:32 +0500

    Время разбивать камни

---
+++ src/presenter/main-presenter.js
@@ -0,0 +1,21 @@
+import CreateForm from "../view/create-form.js";
+import EditForm from "../view/edit-form.js";
+import RoutePoint from "../view/route-point.js";
+import { render } from "../render.js";
+
+export default class MainPresenter{
+    constructor({container}){
+        const tripEventsList = document.createElement('ul');
+        tripEventsList.className = 'trip-events__list';
+        container.appendChild(tripEventsList);
+        this.container = tripEventsList;
+    }
+  
+    init(){
+        render(new EditForm(),this.container);
+        render(new CreateForm(),this.container);
+        for(let i = 0; i < 3; i++){
+            render(new RoutePoint(),this.container);
+        }
+    }
+}
