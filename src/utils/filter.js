83df9aab258e75cb1486994765dfeaeecdcb8186
pavrudakov <rudakov2005@mail.ru>
2025-04-15 17:32:53 +0500

    Большие перемены (часть 1)

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,17 +1,17 @@
 import { filterType } from '../const.js';
- import { isFuturePoint, isPastPoint, isPresentPoint } from './utils';
+import { isFuturePoint, isPastPoint, isPresentPoint } from './utils';
  
- const filter = {
-   [filterType.EVERYTHING]: (points)=> points,
-   [filterType.FUTURE]: (points)=> points.filter((point)=>
-     isFuturePoint(point.date_from)
-   ),
-   [filterType.PRESENT]: (points)=> points.filter((point)=>
-     isPresentPoint(point.date_from, point.date_to)
-   ),
-   [filterType.PAST]: (points)=> points.filter((point)=>
-     isPastPoint(point.date_to)
-   )
- };
+const filter = {
+    [filterType.EVERYTHING]: (points)=> points,
+    [filterType.FUTURE]: (points)=> points.filter((point)=>
+        isFuturePoint(point.dateFrom)
+    ),
+    [filterType.PRESENT]: (points)=> points.filter((point)=>
+        isPresentPoint(point.dateFrom, point.dateTo)
+    ),
+    [filterType.PAST]: (points)=> points.filter((point)=>
+        isPastPoint(point.dateTo)
+    )
+};
  
- export {filter};
+export {filter};
